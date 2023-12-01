52123d7bd33580855e7a9314b102c1f5c484beac
Aman Patel <>
2023-12-01 18:51:49 -0500

    add init code to game engine

--- GameEngine.py
+++ GameEngine.py
@@ -1,5 +1,8 @@
 import random
-
+import os
+import random
+from Rabbit import Rabbit
+from Veggie import Veggie
 from Captain import Captain
 
 
@@ -16,9 +19,41 @@ class GameEngine:
             self.score = 0
 
     def initVeggies(self):
-        # TODO: Initialize vegetables
-        
-        pass
+        """
+        Initializes field with vegetables based on input file.
+
+        Prompts user to enter a file name and reads data from it.
+
+        :return: None. Initializes the field with vegetables.
+        """
+        veggie_file_name = input("Please enter the name of the vegetable point file: ")
+
+        # Validate existence of input file
+        while not os.path.exists(veggie_file_name):
+            veggie_file_name = input(f"{veggie_file_name} does not exist! Please enter the name of the vegetable point file: ")
+
+        # Open and read the veggie file
+        with open(veggie_file_name, 'r') as file:
+            lines = file.readlines()
+
+        # Initialize field with the size specified in the first line
+        field_size = tuple(map(int, lines[0].strip().split(',')[1:]))
+        self.field = [[None for _ in range(field_size[1])] for _ in range(field_size[0])]
+
+        # Process remaining lines to create Veggie objects
+        for line in lines[1:]:
+            parts = line.strip().split(',')
+            veggie = Veggie(parts[0], parts[1], int(parts[2]))
+            self.vegetables.append(veggie)
+
+        # Populate field with vegetables
+        for _ in range(self.__NUMBEROFVEGGIES):
+            while True:
+                x, y = random.randint(0, field_size[0] - 1), random.randint(0, field_size[1] - 1)
+                if self.field[x][y] is None:
+                    self.field[x][y] = random.choice(self.vegetables)
+                    break
+
 
     def initCaptain(self):
         while True:
@@ -29,8 +64,40 @@ class GameEngine:
                 break
 
     def initRabbits(self):
-        # TODO: Initialize rabbits
-        pass
+        """
+        Initializes field with a predefined number of rabbits
+
+        For each rabbit, finds a random, unoccupied location on the field and 
+        places a new rabbit object there.
+
+        :return: None. Populates the field with rabbit objects without returning any value.
+        """
+        # Loop for number of rabbits
+        for _ in range(self.__NUMBEROFRABBITS):
+            # try until rabbit is placed
+            while True:
+                x, y = random.randint(0, len(self.field) - 1), random.randint(0, len(self.field[0]) - 1)
+                if self.field[x][y] is None:
+                    # Create new rabbit
+                    rabbit = Rabbit('R', x, y)
+                    self.rabbits.append(rabbit)
+                    # Place rabbit on field
+                    self.field[x][y] = rabbit
+                    break
+
+    def initializeGame(self):
+        """
+        Initializes game environment by populating it with vegetables, captain, rabbits.
+
+        Calls three methods: `initVeggies` to populate the field with vegetables, 
+        `initCaptain` to place the captain, and `initRabbits` to distribute rabbits on the field. 
+
+        :return: None. Just initializes the game state..
+        """
+        self.initVeggies()
+        self.initCaptain()
+        self.initRabbits()
+    
 
     def initializeGame(self):
         # TODO: Initialize the game
